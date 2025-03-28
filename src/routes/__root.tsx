f5cf3b068529538040ea4e8ed5e81324f7a6df96
titanium-dev <akinkunmioye42@gmail.com>
2025-03-28 22:41:13 +0100

    fix: wrap Outlet and Router Devtools in a div for improved layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,11 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="max-w-7xl mx-auto p-10">
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
-})
+});
