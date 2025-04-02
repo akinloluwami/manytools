aa3d85ac47e89c901798a0fea3c3fe90ea6c2124
titanium-dev <akinkunmioye42@gmail.com>
2025-04-02 14:58:35 +0100

    feat: add HeadContent component to root route for improved SEO and metadata handling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,11 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "react-tooltip/dist/react-tooltip.css";
 
 export const Route = createRootRoute({
   component: () => (
     <div className="max-w-7xl mx-auto p-10">
+      <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
     </div>
