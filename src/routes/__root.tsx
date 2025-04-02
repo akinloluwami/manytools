69808f233c496a258851416e7adc1901ceacf8eb
titanium-dev <akinkunmioye42@gmail.com>
2025-04-02 14:41:35 +0100

    feat: import tooltip CSS for enhanced tooltip styling in root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import "react-tooltip/dist/react-tooltip.css";
 
 export const Route = createRootRoute({
   component: () => (
