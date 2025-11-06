5dc35d046bc5caed928428e919517f234cdee936
akinloluwami <akinkunmioye42@gmail.com>
2025-11-06 06:02:24 +0100

    feat: enhance tool display with 'Coming Soon' badge for WIP tools

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -40,19 +40,31 @@ function App() {
       </header>
       <div className="">
         <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
-          {tools.map((tool, index) => (
-            <Link
-              key={index}
-              to={tool.route}
-              className="p-6 border relative border-black/30 hover:bg-black/5  hover:border-black/20 transition-colors rounded-lg flex items-center justify-between flex-col gap-4"
-            >
-              <tool.icon size={40} iconStyle="LineDuotone" />
-              <p className="text-gray-600 text-center font-semibold text-sm">
-                {tool.title}
-              </p>
-              {/* {tool.wip && <Badge>Coming Soon</Badge>} */}
-            </Link>
-          ))}
+          {tools.map((tool, index) =>
+            tool.wip ? (
+              <div
+                key={index}
+                className="p-6 border relative border-black/30 bg-black/5 opacity-75 cursor-not-allowed rounded-lg flex items-center justify-between flex-col gap-4"
+              >
+                <tool.icon size={40} iconStyle="LineDuotone" />
+                <p className="text-gray-600 text-center font-semibold text-sm">
+                  {tool.title}
+                </p>
+                <Badge className="absolute top-2 right-2">Coming Soon</Badge>
+              </div>
+            ) : (
+              <Link
+                key={index}
+                to={tool.route}
+                className="p-6 border relative border-black/30 hover:bg-black/5  hover:border-black/20 transition-colors rounded-lg flex items-center justify-between flex-col gap-4"
+              >
+                <tool.icon size={40} iconStyle="LineDuotone" />
+                <p className="text-gray-600 text-center font-semibold text-sm">
+                  {tool.title}
+                </p>
+              </Link>
+            ),
+          )}
 
           <button className="p-6 border hover:bg-black/5 border-black/30 rounded-lg flex items-center justify-between flex-col cursor-pointer gap-4">
             <SolarIconSet.WidgetAdd size={40} />
